02d950a54660754280b2dfa66234008196f59c97
Kieran Manzella <kieran.manzella@gmail.com>
2024-06-24 20:48:05 -0500

    inverter working in rust without python integration, won't compile with the pyfunctions etc

--- src/linalg_no_integration.rs
+++ src/linalg_no_integration.rs
@@ -1,84 +1,58 @@
-use pyo3::prelude::*;
-use pyo3::exceptions::PyValueError;
+// pyfunction integration complicates stuff and I'm having trouble tracking down
+// actual sources of bugs. I'm keeping these functions un-integrated for testing
 
 
-#[pyfunction]
-pub fn dot_product(a: Vec<f64>, b: Vec<f64>) -> PyResult<f64> {
-
+fn dot_product(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
     let result: f64 = a.iter()
         .zip(b.iter())
         .map(|(x, y)| x * y)
         .sum();
-    Ok(result)
+  
+    result
 }
 
-
-#[pyfunction]
-pub fn magnitude(a: Vec<f64>) -> PyResult<f64> {
-    let sum_squares: f64 = a.iter()
-        .map(|&x| x * x)
-        .sum();
-    let result: f64 = sum_squares.sqrt();
-    Ok(result)
-}
-
-
-#[pyfunction]
-pub fn normalize(a: Vec<f64>) -> PyResult<Vec<f64>> {
-    let mag: f64 = magnitude(a.clone())?;
-    if mag == 0.0 {
-        return Err(PyErr::new::<PyValueError, _>("Cannot normalize vector with magnitude 0"));
-    }
-
-    let result: Vec<f64> = a.iter().map(|&x| x / mag).collect();
-    Ok(result)
-}
-
-
-#[pyfunction]
-pub fn transpose(matrix: Vec<Vec<f64>>) -> PyResult<Vec<Vec<f64>>> {
-    let mut result = vec![vec![0.0; matrix.len()]; matrix[0].len()];
-
+fn transpose(matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    let mut result = Vec::new();
     for i in 0..matrix[0].len() {
+        let mut row = Vec::new();
         for j in 0..matrix.len() {
-            result[i][j] = matrix[j][i];
+            row.push(matrix[j][i]);
         }
+        result.push(row);
     }
-
-    Ok(result)
+    
+    result
 }
 
 
 fn column_slice(matrix: &Vec<Vec<f64>>, index: usize) -> Vec<f64> {
-    let mut slice: Vec<f64> = vec![0.0; matrix.len()];
-    for i in 0..matrix.len() {
-        slice[i] = matrix[i][*&index];
-    }
+  let mut slice: Vec<f64> = vec![0.0; matrix.len()];
+  for i in 0..matrix.len() {
+      slice[i] = matrix[i][index];
+  }
 
-    slice
+  slice
 }
 
 
-#[pyfunction]
-pub fn matmul(a: Vec<Vec<f64>>, b: Vec<Vec<f64>>) -> PyResult<Vec<Vec<f64>>> {  
+fn matmul(a: &Vec<Vec<f64>>, b: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {  
   let mut result = vec![vec![0.0; a.len()]; b[0].len()];
   for i in 0..a.len() {
       let a_row = a[i].clone();
       for j in 0..b[i].len() {
           let b_column = column_slice(&b, j);
-          result[i][j] = dot_product(a_row.clone(), b_column)?;
+          result[i][j] = dot_product(&a_row, &b_column)
       }
   }
-
-  Ok(result)
+  result
 }
 
-
 fn get_matrix_minor(m: &Vec<Vec<f64>>, i: usize, j: usize) -> Vec<Vec<f64>> {
+// return matrix with rows excluding index i, columns excluding index j
   let mut rows: Vec<Vec<f64>> = m[0..i].to_vec();
   let mut following_rows: Vec<Vec<f64>> = m[i+1..].to_vec();
   rows.append(&mut following_rows);
-  let mut minor = Vec::new();
+  let mut minor = vec![];
   for i in rows.iter_mut() {
     i.remove(j);
     minor.push(i.to_vec());
@@ -86,12 +60,10 @@ fn get_matrix_minor(m: &Vec<Vec<f64>>, i: usize, j: usize) -> Vec<Vec<f64>> {
   minor
 }
 
-
 fn get_determinant(m: &Vec<Vec<f64>>) -> f64 {
   if m.len() == 2 {
     return m[0][0] * m[1][1] - m[0][1] * m[1][0];
   }
-  
   let mut determinant: f64 = 0.0;
   for c in 0..m.len() {
     let minor = get_matrix_minor(&m, 0, c);
@@ -102,18 +74,15 @@ fn get_determinant(m: &Vec<Vec<f64>>) -> f64 {
 }
 
 
-#[pyfunction]
-pub fn get_matrix_inverse(m: Vec<Vec<f64>>) -> PyResult<Vec<Vec<f64>>> {
+
+pub fn get_matrix_inverse(m: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
   let determinant = get_determinant(&m);
-  if determinant == 0.0 {
-        return Err(pyo3::exceptions::PyValueError::new_err("Matrix is singular and cannot be inverted."));
-  }
-  
+
   if m.len() == 2 {
     let mut inverse = Vec::new();
-    inverse.push([m[1][1] / determinant, -1.0 * m[0][1] / determinant].to_vec());
-    inverse.push([-1.0 * m[1][0] / determinant, m[0][0] / determinant].to_vec());
-    return Ok(inverse);
+    inverse.push([m[1][1] / determinant, -1.0 * m[0][1] / determinant].to_vec());    
+    inverse.push([-1.0 * m[1][0] / determinant, m[0][0] / determinant].to_vec());    
+    return inverse;
   }
 
   let mut cofactors = Vec::new();
@@ -136,5 +105,19 @@ pub fn get_matrix_inverse(m: Vec<Vec<f64>>) -> PyResult<Vec<Vec<f64>>> {
     }
     inverse.push(inv_row);
   }
-  Ok(inverse)
+  inverse
+}
+
+fn main() {
+    let a = vec![1.0, 2.0, 3.0];
+    let b = vec![4.0, 2.0, 6.0];
+    let c = vec![7.0, 8.0, 12.0];
+    let m = vec![a.clone(), b.clone(), c.clone()];
+
+    let m_inv = get_matrix_inverse(m.clone());
+
+    let i_test = matmul(&m, &m_inv);
+    for row in i_test.iter() {
+      println!("{:?}", row);
+    }
 }
